df2c5c5ec27b2ab2c9f523997cfbf0ed1437f11a
wuhui.zhang <ziegler@nd.com.cn>
2021-08-31 16:51:41 +0800

    update userinfo

--- user_service.thrift
+++ user_service.thrift
@@ -2,11 +2,16 @@ namespace rb bush
 
 
 struct UserInfo {
-  1: i32 id
-  2: string name
+  1:required i32 id
+  2:required string name
 }
 
 
-service UserService{
+struct RcpResponse {
+  1: required i32 code
+  2: UserInfo userInfo
+}
+
+service UserService {
    string getUser(1:i32 id)
 }
